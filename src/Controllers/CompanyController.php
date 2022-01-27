0a7fc109599df13f5562790c164e7fba474a692c
m_ronald <michael.ronald@allycode.com.br>
2022-01-26 22:05:45 -0300

    applying business rule in the store method

--- src/Controllers/CompanyController.php
+++ src/Controllers/CompanyController.php
@@ -52,9 +52,59 @@ class CompanyController extends Controller
         $companyAddress->country = $data->address->country;
         $companyAddress->save();
 
+        $message = $this->validateRoles($company);
         $response = [
-            'message' => 'created successfully'
+            'status' => 'created successfully',
+            'message' => $message,
+            'register_id' => $company->id,
         ];
+
         $this->returnAPIResult($response);
+    }
+
+    private function validateRoles(Company $company): string
+    {
+        $message = '';
+
+        if ($company->state == 'Goiás' && $company->register_type == 'Variante') {
+            $message = 'Mandar equipe de campo';
+        }
+
+        if (
+            $company->state == 'São Paulo' &&
+            $company->register_type == 'Variante' &&
+            $company->person_type == 'Física'
+        ) {
+            $message = 'Reavaliar em 2 meses';
+        }
+
+        if (
+            $company->state == 'Ceará' &&
+            $company->register_type == 'Variante' &&
+            $company->person_type == 'Física' &&
+            !empty($company->notes)
+        ) {
+            $message = 'Possível violação do tratado Beta';
+        }
+
+        if (
+            $company->state == 'Tocantins' &&
+            $company->register_type == 'Variante' &&
+            $company->person_type == 'Física' &&
+            !empty($company->notes)
+        ) {
+            $message = 'Possível violação do tratado Celta';
+        }
+
+        if (
+            $company->state == 'Amazonas' &&
+            $company->register_type == 'Variante' &&
+            $company->person_type == 'Física' &&
+            !empty($company->notes)
+        ) {
+            $message = 'Possível violação do tratado Alpha';
+        }
+
+        return $message;
     }
 }
